7bf9a4eab4ec34a87f1d65e22c6047786e206d06
George Miliaresis <miliaresis.g@gmail.com>
2018-03-06 16:18:42 +0200

    Add files via upload

--- clu_data_headers.py
+++ clu_data_headers.py
@@ -78,6 +78,30 @@ def dataCL1(tiff_import_options):
     return (xy, ML, x2, x3, Lmin, Lmax, LDIR, T)
 
 
+def dataCL3(tiff_import_options):
+    """LST residual 1-km, 8-day """
+    print('\n--> LST residual 1-km, 8-day')
+# Main figure labels (title, x-axis, y-axis)
+    ML = ['LST, deg', 'Y km', 'X km']
+    # Geograhic extent (X-LON-min, X-LON-max, Y-LAT-min, Y-LAT-max)
+    xy = [-11119.5, -10007.5, 3333.9, 4447.8]
+# tics for axes of figures and cross-correlation matrix
+    x2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+          '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+          '25', '26', '27', '28', '39', '30', '31', '32', '33', '34', '35',
+          '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46']
+    x3 = x2
+# Histograms domain for data (eg. DEM) & reconstructed data (eg. DEM)
+    Lmin = -25
+    Lmax = 25
+# PIL Library is used for TIF file import
+    T = tiff_import_options[0]
+# Sub-directory for image files or vector matrix
+    LDIR = 'dataCL3'
+    phead(xy, ML, x2, x3, Lmin, Lmax, LDIR, T)
+    return (xy, ML, x2, x3, Lmin, Lmax, LDIR, T)
+
+
 def dataCL4(tiff_import_options):
     """ALOS, SRTM, ASTER DEMs: residual elevations """
     print('\n--> ALOS, SRTM, ASTER GDEMs residual H, 1s lat lon WGS84,EGM96')
